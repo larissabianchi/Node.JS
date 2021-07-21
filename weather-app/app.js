dddb689a8e9eb8d71c9e625501de8dea95379f67
larissabianchi <75446880+larissabianchi@users.noreply.github.com>
2021-07-21 20:47:39 -0300

    Update app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,15 +1,17 @@
 const request = require('postman-request')
 const color = require('chalk')
 
-const url = 'http://api.weatherstack.com/current?access_key=f90b973d4666ad4d68e7938fb0babc6a&query=37.8267,-122.4233&units=f'
+const weatherURL = 'http://api.weatherstack.com/current?access_key=f90b973d4666ad4d68e7938fb0babc6a&query=37.8267,-122.4233&units=f'
 
-request({url: url, json: true},(error, response) => {
-    //console.log(response.body.current)
-    const temperature = response.body.current.temperature
-    const feelsLike = response.body.current.feelslike
-    const weatherDesc = response.body.current.weather_descriptions
+request({url: weatherURL, json: true},(error, response) => {
+    console.log(color.blue.bold('Today is '+response.body.current.weather_descriptions))
+    console.log('It is currently '+ response.body.current.temperature +' degress out.')
+    console.log('It feels like '+ response.body.current.feelslike +' degress out.')
+})
 
-    console.log(color.blue.bold('Today is '+weatherDesc))
-    console.log('It is currently '+ temperature +' degress out.')
-    console.log('It feels like '+ feelsLike +' degress out.')
+const LocationURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGFyaXNzYWJpYW5jaGkiLCJhIjoiY2tyZTNieXRoNGVjMDJzcnhoaW44enE5ZyJ9._uuTjrp6AXueJSiAXfS97A&limit=1'
+
+request({url: LocationURL, json: true},(error, response) => {
+    console.log(color.magenta.bold('Latitude: ')+ response.body.features[0].center[1])
+    console.log(color.magenta.bold('Longitude: ')+ response.body.features[0].center[0])
 })
