6a171eca6fdb15466fa0a07084ed397ded52c849
larissabianchi <75446880+larissabianchi@users.noreply.github.com>
2022-01-11 17:16:29 -0300

    Application Deployment

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side JavaScript file is loaded!')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const resultMessage = document.querySelector('#resultMessage')
@@ -13,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 resultMessage.textContent = data.error
